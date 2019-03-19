4dfaa5d4e04a9e4fdaa8e0db20ec6289aeeeb4e0
fakhrurraziandi <fakhrurrazi.andi@gmail.com>
2019-03-20 01:24:41 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<link rel="stylesheet" href="./dist/css/main.css">
+	<link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	
+	<nav class="navbar navbar-expand-md navbar-light bg-light">
+		<div class="container">
+			<a class="navbar-brand" href="#">
+				<img src="./dist/img/logo.png" alt="" style="width: 150px;">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Tentang Kami</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Akad Kredit Syariah</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Pengajuan Online</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Beli Motor</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Kontak Kami</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Login</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	
+	<div class="jumbotron text-white" style="background-image: url('./dist/img/banner.jpg'); background-size: cover; background-position: center center; padding: 200px 0;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h1 style="letter-spacing: 10px;">DANA SYARIAH</h1>
+					<h2>AJUKAN KREDIT SEKARANG</h2>
+					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, architecto.</p>
+					<a class="btn btn-lg btn-primary" href="/docs/4.3/components/navbar/" role="button">KLIK DISINI</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<section class="py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2>Apa itu Dana Syariah?</h2>
+					<p>Menyediakan Layanan Investasi Syariah dan Pembiayaan Syariah bagi pemilik Usaha ataupun Perorangan, dengan tujuan mendapatkan manfaat dan Bagi Hasil yang Halal serta terhindar dari unsur Maisir, Gharar dan Riba.</p>
+				</div>
+				<div class="col-md-6">
+					<div class="card border-primary" style="border-width: 20px;">
+						<iframe width="100%" height="315" src="https://www.youtube.com/embed/OfIQW6s1-ew" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h3>PRODUK DAN LAYANAN DANA SYARIAH</h3>
+					<p>Sebagai salah satu perusahaan pembiayaan terbesar di Indonesia, PT. Bussan Auto Finance menyediakan beberapa produk pembiayaan dengan prinsip syariah yang telah disetujui oleh Otoritas Jasa Keuangan (OJK) dan Dewan Syariah Nasional - Majelis Ulaman Indonesia (DSN-MUI), di antaranya adalah:</p>
+
+					<ol>
+						<li>
+							<p>Pembiayaan Sepeda Motor</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere voluptates esse molestias atque voluptate! Atque quae minima et reprehenderit, ipsum perspiciatis ratione facilis quas nemo officiis maxime ipsam voluptatibus, maiores.</p>
+						</li>
+
+						<li>
+							<p>Dana Syariah</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere voluptates esse molestias atque voluptate! Atque quae minima et reprehenderit, ipsum perspiciatis ratione facilis quas nemo officiis maxime ipsam voluptatibus, maiores.</p>
+						</li>
+					</ol>
+
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="accordion" id="accordionExample">
+						<div class="card border-top-0 border-left-0 border-right-0 pb-2">
+							<div class="card-header bg-white" id="headingOne">
+								<h2 class="mb-0">
+									<button class="btn btn-link collapsed text-secondary" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+										OBJEK PEMBIAYAAN DANA SYARIAH
+									</button>
+								</h2>
+							</div>
+
+							<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+						<div class="card border-top-0 border-left-0 border-right-0 pb-2">
+							<div class="card-header bg-white" id="headingTwo">
+								<h2 class="mb-0">
+									<button class="btn btn-link collapsed text-secondary" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+										PELAKSANAAN PEMBIAYAAN DI SANA SYARIAH
+									</button>
+								</h2>
+							</div>
+							<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+						<div class="card border-top-0 border-left-0 border-right-0 pb-2">
+							<div class="card-header bg-white" id="headingThree">
+								<h2 class="mb-0">
+									<button class="btn btn-link collapsed text-secondary" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+										PELAKSANAAN PEMBIAYAAN KONVENSIONAL DAN SYARIAH
+									</button>
+								</h2>
+							</div>
+							<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="py-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<img src="./dist/img/why.jpg" class="img-fluid" alt="">
+				</div>
+				<div class="col-md-6">
+					<h2>Mengapa harus dana syariah?</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt asperiores fugiat eum. Et, reprehenderit, voluptates! Sequi, explicabo ipsam labore itaque doloribus ducimus reprehenderit ratione recusandae unde laboriosam, libero est harum.</p>
+					<p>Maiores aliquid cumque ipsum. Et quas deserunt quidem dolor libero ipsa voluptatem, nesciunt expedita rem provident reiciendis nihil harum, iusto quis. Maiores laudantium ipsa veritatis molestias corporis adipisci mollitia neque?</p>
+					
+					<div class="row">
+						<div class="col-md-6">
+							<p class="d-flex align-items-center">
+								<i class="fa fa-check-circle-o fa-2x text-primary px-2"></i> <span>Lorem ipsum dolor sit amet.</span>
+							</p>
+						</div>
+						<div class="col-md-6">
+							<p class="d-flex align-items-center">
+								<i class="fa fa-check-circle-o fa-2x text-primary px-2"></i> <span>Lorem ipsum dolor sit amet.</span>
+							</p>
+						</div>
+						<div class="col-md-6">
+							<p class="d-flex align-items-center">
+								<i class="fa fa-check-circle-o fa-2x text-primary px-2"></i> <span>Lorem ipsum dolor sit amet.</span>
+							</p>
+						</div>
+						<div class="col-md-6">
+							<p class="d-flex align-items-center">
+								<i class="fa fa-check-circle-o fa-2x text-primary px-2"></i> <span>Lorem ipsum dolor sit amet.</span>
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="py-5 text-white" style="background-image: url('./dist/img/subscribe.jpg'); background-size: cover; background-position: center center; ">
+		<div class="py-5 container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2 class="h1">Cari Pinjaman?</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor commodi, perferendis, qui ut assumenda eius perspiciatis optio. Doloribus in, odio eius consectetur repudiandae nam odit id, ea, sunt, quas minima.</p>
+
+					<form class="form-inline bg-white d-flex align-items-center mt-5" style="padding: 15px; border-radius: 50px;">
+						
+						<div class="form-group mx-sm-3" style="flex: 1;">
+							<label for="inputPassword2" class="sr-only">Password</label>
+							<input type="password" class="form-control w-100 border-0" id="inputPassword2" placeholder="Masukkan E-Mail Anda">
+						</div>
+						<button type="submit" class="btn btn-primary mb-2 rounded-pill px-5">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="py-5" style="background-image: url('dist/img/bottom-banner.jpg'); background-size: cover; background-position: center center;">
+		<div class="container py-5 my-5">
+			<div class="row py-5 my-5">
+				<div class="col-md-6 ml-auto">
+					<h2 class="text-white" style="text-shadow: 1px 0px 13px green;">PEMBIAYAAN MOTOR DANA SYARIAH</h2>
+					<p><a href="#" class="btn btn-primary">Read More</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="py-5">
+		<div class="container">
+			<div class="row mb-5">
+				<div class="col-md-6 offset-md-3">
+					<div class="card">
+						<div class="card-body text-center">
+							<img src="./dist/img/customer.jpg" class="img-fluid mb-3" alt="">
+							<img src="./dist/img/logo.png" style="width: 150px;" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h2 class="h1 text-primary mb-3">Customer DSM</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eius id eaque soluta, inventore placeat, harum reprehenderit libero similique, consequatur saepe expedita! Sit, ea. Ex, molestiae autem eius velit, excepturi ipsum eveniet. Beatae iusto consequuntur ullam temporibus sint quae esse accusantium nisi pariatur repudiandae facilis atque at dicta, id, assumenda magnam dolor repellat totam corporis. Labore nemo delectus quasi iusto nam sit, sequi accusantium molestias animi, officiis tempora ad soluta.</p>
+					<p><a href="" class="btn btn-primary">Lihat Galeri</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer class="py-5 bg-primary text-white">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<h2>Dana Syariah</h2>
+					<p>Kantor Pusat <br>Menara Mulia Building <br>Jl. Gatot Subroto Kav.9-11 <br>Jakarta 12930</p>
+				</div>
+				<div class="col-md-3">
+					<h2>Dukungan</h2>
+					<ul class="list-unstyled">
+						<li><a href="#" class="text-white">Karir</a></li>
+						<li><a href="#" class="text-white">Tanya Kami</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-6">
+							<img src="./dist/img/mobile-phone.png" class="img-fluid" alt="">		
+						</div>
+						<div class="col-md-6">
+							<h6>Semakin Mudah dan Nyaman dengan DANA SYARIAH Mobile</h6>
+							<p><img src="./dist/img/playstore-button.png" style="width: 200px;" alt=""></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
+
+
+	</footer>
+
+	<footer class="gradient-theme text-white py-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 py-2">
+					2019 - Dana Syariah
+				</div>
+				<div class="col-md-6 py-2 text-right">
+					Terdaftar dan diawasi Otoritas Jasa Keuangan
+				</div>
+			</div>
+		</div>
+	
+	</footer>
+	
+
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
